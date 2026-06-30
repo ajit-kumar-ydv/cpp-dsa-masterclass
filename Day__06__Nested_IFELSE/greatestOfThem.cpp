e57d372cd1725817b9beb0315afe5f405da309b7
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-06-30 21:47:35 +0530

    Nested IfElse

---
+++ Day__06__Nested_IFELSE/greatestOfThem.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Take 3 positive integers input and print the greatest of them
+  
+}
---
+++ Day__06__Nested_IFELSE/isAreaGreaterThanPerimeter.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //Take length and breadth of rectangle as input and write a program to find whether 
+  // the area of the rectangle is greater than its perimeter
+}
