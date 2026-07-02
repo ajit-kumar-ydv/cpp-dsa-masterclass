552557ad37713ac2b1d09882caea5b17c65828f6
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-07-02 13:04:22 +0530

    TERNARY

--- Day__07_TernaryOperator/oddOrEven.cpp
+++ Day__07_TernaryOperator/oddOrEven.cpp
@@ -1,1 +1,9 @@
-#incl
+#include<iostream>
+using namespace std;
+int main(){
+  //Take positive integer input and tell if it is odd or even
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  n % 2 == 0 ? cout << "Even" : cout << "Odd";
+}
