3fc8de976a8c9f5b618123bec5323a18dd9f1cbc
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-07-03 13:05:31 +0530

    FOR LOOPS

---
+++ Day__08_Loops/ForLoop/oddNumDivisibleBy3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout << "Enter the last number: ";
+  cin >> n;
+  for (int i = 1; i <= n; i++){
+    if (i % 3 == 0)
+      cout << i << " ";
+  }
+}
---
+++ Day__08_Loops/ForLoop/print1to100.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout << "Enter your last count number: ";
+  cin >> n;
+  for (int i = 1; i <= n; i++){
+    cout << i << " ";
+  }
+}
