82e09db8912d2cecd853f1c941d6df388b9f932c
AjitKumarDev <ajitkumar.itian@gmail.com>
2026-07-04 10:33:21 +0530

    For Loops Completed

--- Day__08_Loops/ForLoop/evenNumberFrom1To100.cpp
+++ Day__08_Loops/ForLoop/evenNumberFrom1To100.cpp
@@ -7,4 +7,23 @@ int main(){
   for (int i = 2; i <= n; i+=2){
     cout << i << " ";
   }
+
+   // 100 iterations
+    // for(int i=1;i<=100;i++){
+    //     if(i%2 == 0) cout<<i<<" ";
+    // }
+
+    // // 50 iterations
+    // for(int i=2;i<=100;i+=2){
+    //     cout<<i<<" ";
+    // }
+
+    // 50 iterations
+  cout << endl
+       << endl;
+  for (int i = 1; i <= 50; i++)
+  {
+    cout << i * 2 << " ";
+  }
+
 }
